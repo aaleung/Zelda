ae774d4de08df49160d45c81762270ffbb79b752
aaleung <aaleung@umich.edu>
2017-01-16 13:41:34 -0500

    Initial Commit
    Part of the tutorial implementation up to ~35 minutes

---
+++ Assets/Hud.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour {
+
+    public Text rupee_text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int num_player_rupees = player_control.instance.rupee_count;
+        rupee_text.text = "Rupees: " + num_player_rupees.ToString();
+	}
+}
---
+++ Assets/player_control.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_control : MonoBehaviour {
+
+    public float walking_velocity = 1.0f;
+    public int rupee_count = 0;
+
+    public static player_control instance;
+
+	// Use this for initialization
+	void Start () {
+        if(instance != null)
+        {
+            Debug.LogError("Multiple Link objects detected! There is more than one Link.");
+        }
+        instance = this; //Singleton
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal_input = Input.GetAxis("Horizontal");
+        float vertical_input = Input.GetAxis("Vertical");
+
+        if (horizontal_input != 0.0f)
+        {
+            vertical_input = 0.0f;
+        }
+        GetComponent<Rigidbody>().velocity = new Vector3(horizontal_input, vertical_input, 0) * walking_velocity;
+	}
+
+    void OnTriggerEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Rupee")
+        {
+            
+            Destroy(coll.gameObject);
+            rupee_count++;
+        }
+        
+    }
+}
